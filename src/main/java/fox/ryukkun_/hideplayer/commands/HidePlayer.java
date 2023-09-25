b00234413abe89390ff3cbf187d1f69ffc9e63c6
Ryukkun <tkytwins2@gmail.com>
2023-09-26 04:05:54 +0900

    ⚡️HidePlayer command 🐜hideした状態で抜けたらアイテムがバグる。

--- src/main/java/fox/ryukkun_/hideplayer/commands/HidePlayer.java
+++ src/main/java/fox/ryukkun_/hideplayer/commands/HidePlayer.java
@@ -13,70 +13,138 @@ public class HidePlayer extends SubCommandTPL{
     public HidePlayer() {
         subCommands.put("reload", new Reload());
         subCommands.put("set-interval", new SetInterval());
-        subCommands.put("set-itemId", new SetItemId());
-        subCommands.put("check-name", new CheckName());
-        subCommands.put("set-name", new SetName());
+        subCommands.put("set-item", new SetItem());
         subCommands.put("set-message", new SetMessage());
     }
 
-    private static class SetMessage extends SubCommandTPL {
-        public SetMessage() {
-            subCommands.put("hide", new HideMessage());
-            subCommands.put("show", new ShowMessage());
+    private static void configSet(CommandSender sender, String path, String[] args) {
+        configSet(sender, path, (0 == args.length) ? "" : args[0]);
+    }
+    private static void configSet(CommandSender sender, String path, String arg) {
+        main.getFileConfig().set(path, arg);
+        main.getPlugin().saveConfig();
+        MCLogger.sendMessage(sender, MCLogger.Level.Success, path+"を \""+arg+"\" に設定しました。");
+    }
+
+
+    private static class SetItem extends SubCommandTPL {
+        public SetItem() {
+            subCommands.put("change_item", new ChangeItem());
+            subCommands.put("hide", new SetItemTPL("hide"));
+            subCommands.put("show", new SetItemTPL("show"));
         }
 
-        private static class HideMessage implements CMD {
-            @Override
-            public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-                String id = "message.hide";
-                String arg;
-                if (0 == args.length) arg = "";
-                else arg = args[0];
-                main.getFileConfig().set(id, args[0]);
-                main.getPlugin().saveConfig();
-                MCLogger.sendMessage(sender, MCLogger.Level.Success, id+"を \""+args[0]+"\" に設定しました。");
-                return true;
-            }
+        private static class ChangeItem implements CMD {
+            private static final String[] booleanList = new String[]{"true", "false"};
 
             @Override
-            public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-                return null;
-            }
-        }
-
-        private static class ShowMessage implements CMD {
-            @Override
             public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-                String id = "message.show";
-                if (0 == args.length) {
-                    MCLogger.sendMessage(sender, MCLogger.Level.Success, id+"は \""+main.getFileConfig().get(id)+"\" です。");
+                boolean res;
+                try {
+                    res = Boolean.parseBoolean(args[0]);
+
+                } catch (Exception e) {
+                    MCLogger.sendMessage(sender, MCLogger.Level.Error, "true または false を入力して下さい");
                     return true;
                 }
-                main.getFileConfig().set(id, args[0]);
-                main.getPlugin().saveConfig();
-                MCLogger.sendMessage(sender, MCLogger.Level.Success, id+"を \""+args[0]+"\" に設定しました。");
+
+                configSet(sender, "item.change_item", Boolean.toString(res));
                 return true;
             }
 
             @Override
             public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-                return null;
+                List<String> tabList = new ArrayList<>();
+                for (String str : booleanList) {
+                    if (args[0].isEmpty() || str.startsWith(args[0])) {
+                        tabList.add(str);
+                    }
+                }
+                return tabList;
+            }
+        }
+
+        private static class SetItemTPL extends SubCommandTPL {
+            public SetItemTPL(String path) {
+                path = "item."+path+".";
+
+                subCommands.put("id", new SetId(path+"id"));
+                subCommands.put("name", new SetStringTPL(path+"name"));
+                subCommands.put("lour", new SetStringTPL(path+"lour"));
+            }
+
+            private static class SetId implements CMD {
+                private final String path;
+                public SetId(String path) {
+                    this.path = path;
+                }
+                @Override
+                public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+                    if (0 == args.length) {
+                        MCLogger.sendMessage(sender, MCLogger.Level.Error, "値を入力してください。");
+                        return true;
+                    }
+
+                    String arg = args[0];
+                    if (Material.matchMaterial(arg) == null) {
+                        MCLogger.sendMessage(sender, MCLogger.Level.Error, "アイテムを認識できませんでした。");
+                        return true;
+                    }
+
+                    configSet(sender, path, arg);
+                    return true;
+                }
+
+                @Override
+                public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+                    return itemMaterials(args[0]);
+                }
+
+                private static List<String> itemMaterials(String prefix) {
+                    List<String> tabList = new ArrayList<>();
+                    for (Material material : Material.values()) {
+                        if (!material.isItem()) continue;
+                        String name = material.name();
+
+                        if (prefix.isEmpty() || name.startsWith(prefix)) {
+                            tabList.add(name);
+                        }
+                    }
+                    return tabList;
+                }
             }
         }
     }
 
 
-    private static class SetName implements CMD {
+
+    private static class SetMessage extends SubCommandTPL {
+        public SetMessage() {
+            subCommands.put("hide", new SetStringTPL("message.hide"));
+            subCommands.put("show", new SetStringTPL("message.show"));
+            subCommands.put("prefix", new Prefix());
+        }
+
+
+        private static class Prefix extends SubCommandTPL {
+            public Prefix() {
+                String path = "message.prefix.";
+                subCommands.put("success", new SetStringTPL(path+"success"));
+                subCommands.put("warning", new SetStringTPL(path+"warning"));
+                subCommands.put("error", new SetStringTPL(path+"error"));
+            }
+        }
+    }
+
+
+    private static class SetStringTPL implements CMD {
+        private final String path;
+        public SetStringTPL(String path) {
+            this.path = path;
+        }
         @Override
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            String id = "item.name";
-            if (0 == args.length) {
-                MCLogger.sendMessage(sender, MCLogger.Level.Success, id+"は \""+main.getFileConfig().get(id)+"\" です。");
-                return true;
-            }
-            main.getFileConfig().set(id, args[0]);
-            main.getPlugin().saveConfig();
-            MCLogger.sendMessage(sender, MCLogger.Level.Success, id+"を \""+args[0]+"\" に設定しました。");
+            configSet(sender, path, args);
             return true;
         }
 
@@ -87,77 +155,6 @@ public class HidePlayer extends SubCommandTPL{
     }
 
 
-
-    private static class CheckName implements CMD {
-        private static final String[] booleanList = new String[]{"true", "false"};
-
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            boolean res;
-            try {
-                res = Boolean.parseBoolean(args[0]);
-
-            } catch (Exception e) {
-                MCLogger.sendMessage(sender, MCLogger.Level.Error, "true または false を入力して下さい");
-                return true;
-            }
-
-            main.getFileConfig().set("item.check_name", res);
-            main.getPlugin().saveConfig();
-            MCLogger.sendMessage(sender, MCLogger.Level.Success, "item.check_nameを \""+res+"\" に設定しました。");
-            return true;
-        }
-
-        @Override
-        public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-            List<String> tabList = new ArrayList<>();
-            for (String str : booleanList) {
-
-                if (args[0].isEmpty() || str.startsWith(args[0])) {
-                    tabList.add(str);
-                }
-            }
-            return tabList;
-        }
-    }
-
-
-
-    private static class SetItemId implements CMD {
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            if (0 == args.length) {
-                MCLogger.sendMessage(sender, MCLogger.Level.Error, "値を入力してください。");
-                return true;
-            }
-
-            String arg = args[0];
-            if (Material.matchMaterial(arg) == null) {
-                MCLogger.sendMessage(sender, MCLogger.Level.Error, "アイテムを認識できませんでした。");
-                return true;
-            }
-
-            main.getFileConfig().set("item.id", arg);
-            main.getPlugin().saveConfig();
-            MCLogger.sendMessage(sender, MCLogger.Level.Success, "Item.idを \""+arg+"\" に設定しました。");
-            return true;
-        }
-
-        @Override
-        public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-            List<String> tabList = new ArrayList<>();
-            for (Material material : Material.values()) {
-                if (!material.isItem()) continue;
-                String name = material.name();
-
-                if (args[0].isEmpty() || name.startsWith(args[0])) {
-                    tabList.add(name);
-                }
-            }
-            return tabList;
-        }
-
-    }
 
 
     private static class SetInterval implements CMD {
@@ -175,9 +172,7 @@ public class HidePlayer extends SubCommandTPL{
                 MCLogger.sendMessage(sender, MCLogger.Level.Error, "少数を入力してください。");
                 return true;
             }
-            main.getFileConfig().set("interval", arg);
-            main.getPlugin().saveConfig();
-            MCLogger.sendMessage(sender, MCLogger.Level.Success, "Intervalを \""+arg+"\" に設定しました。");
+            configSet(sender, "interval", Double.toString(arg));
             return true;
         }
 
--- src/main/java/fox/ryukkun_/hideplayer/events/PlayerJoin.java
+++ src/main/java/fox/ryukkun_/hideplayer/events/PlayerJoin.java
@@ -2,10 +2,16 @@ package fox.ryukkun_.hideplayer.events;
 
 import fox.ryukkun_.hideplayer.ControlVisibility;
 import fox.ryukkun_.hideplayer.main;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
 
 public class PlayerJoin implements Listener {
     @EventHandler
@@ -15,5 +21,28 @@ public class PlayerJoin implements Listener {
         for (Player hidingPlayer : ControlVisibility.getHidingPlayer()) {
             hidingPlayer.hidePlayer(main.getPlugin(), player);
         }
+
+        FileConfiguration config = main.getFileConfig();
+        if (config.getBoolean("item.change_item")) {
+            Material showMaterial = Material.matchMaterial(config.getString("item.show.id"));
+            Material hideMaterial = Material.matchMaterial(config.getString("item.hide.id"));
+            String showName = config.getString("item.show.name");
+            String hideName = config.getString("item.hide.name");
+
+            for (ItemStack is : player.getInventory()) {
+                if (!is.getType().equals(showMaterial)) continue;
+                ItemMeta im = is.getItemMeta();
+                if (im.hasDisplayName() || !showName.isEmpty()) {
+                    if (!im.getDisplayName().equals( showName)) continue;
+                }
+
+                is.setType(hideMaterial);
+                im.setDisplayName(hideName);
+                im.setLore(Arrays.asList(config.getString("item.hide.lure").split("\n")));
+                is.setItemMeta(im);
+            }
+        }
+
+
     }
 }
